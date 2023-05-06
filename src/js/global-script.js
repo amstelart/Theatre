ad13ceb93484df45f846dcd157b91c0a3acdaae4
amstelart <amsteldroid@gmail.com>
2023-05-07 02:14:32 +0300

    02 commit

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -16,6 +16,31 @@ $( document ).ready(function() {
     center: false,
   });
 
+  $("#other-products").owlCarousel({
+    items: 4,
+    nav: true,
+    margin: 20,
+    loop: true,
+    center: false,
+    responsive : {
+      0 : {
+        items: 2,
+        nav: true,
+        loop: true,
+        center: true,
+      },
+      480 : {
+        items: 2,
+      },
+      768 : {
+        items: 3,
+      },
+      992 : {
+        items: 4,
+      }
+    }
+  });
+
   $("#news-carousel").owlCarousel({
     items: 4,
     nav: true,
