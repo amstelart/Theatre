4d791c9c8e7062c1d12cdfce4b600015324fc5f0
amstelart <amsteldroid@gmail.com>
2023-05-08 19:55:11 +0300

    03 commit

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -65,12 +65,53 @@ $( document ).ready(function() {
       }
     }
   });
+
+  $("#project-carousel").owlCarousel({
+    items: 4,
+    nav: true,
+    margin: 20,
+    loop: true,
+    center: false,
+    responsive : {
+      0 : {
+        items: 2,
+        nav: true,
+        loop: true,
+        center: true,
+      },
+      480 : {
+        items: 2,
+      },
+      768 : {
+        items: 2,
+      },
+      992 : {
+        items: 2,
+      }
+    }
+  });
 });
 
 // Изоляция без jQuery
-// (function(){
-//   // code
-// }());
+(function(){
+  // code
+  const swiper = new Swiper(".view-slider-thumbnail", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    watchSlidesProgress: true,
+  });
+  const swiper2 = new Swiper(".view-slider", {
+    spaceBetween: 10,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+}());
 
 // На проекте нет jQuery, но хочется $( document ).ready...
 // function ready(fn) {
